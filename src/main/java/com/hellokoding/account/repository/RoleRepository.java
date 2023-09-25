3c8c14471c65ffb0e5d8e39d808e5beb88a51ae7
KUNAL RAJESH KOREWAR <kunal.korewar@gmail.com>
2023-09-25 07:55:39 +0530

    2nd

--- src/main/java/com/hellokoding/account/repository/RoleRepository.java
+++ src/main/java/com/hellokoding/account/repository/RoleRepository.java
@@ -3,5 +3,5 @@ package com.hellokoding.account.repository;
 import com.hellokoding.account.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long>{
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
